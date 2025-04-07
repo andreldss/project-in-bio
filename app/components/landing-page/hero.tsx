5e7d846fbd19ede2b2ede1b36b8bf2f8b7cd9fa9
Andre Luiz Dos Santos <001005763@copobras.local>
2025-04-07 08:06:42 -0300

    added user-card

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,10 +1,11 @@
 import TextInput from "../ui/text-input"
 import Button from "../ui/button"
+import UserCard from "../commons/user-card"
 
 
 export default function Hero() {
     return (
-        <div className="flex border">
+        <div className="flex h-screen">
             <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">
                     Seus projetos e redes sociais em um único link
@@ -20,8 +21,9 @@ export default function Hero() {
                     <Button>Criar agora</Button>
                 </div>
             </div>
-            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%, #4B2DBB, transparent_55%)]">
+            <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
                 <div className="relative">
+                    <UserCard />
                     <div className="absolute -bottom-[7%] -right-[45%]">
 
                     </div>
